490c25814365d810f56cea6f6cf7c79e64916d81
seaho5599 <seaho5599@gmail.com>
2022-08-25 18:00:42 +0900

    상단 header 작업 진행중

--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
+$(document).ready(function(){
+
+  let moreBtn = $('.more-btn');
+  let moreBtnList = $('.more-btn-list');
+  moreBtn.mouseenter(function(){
+    moreBtnList.stop().slideDown(100);
+  })
+  moreBtnList.mouseleave(function(){
+    moreBtnList.stop().slideUp(200);
+  })
+  let login = $('.login')
+  let signBox = $('.signbox')
+  login.mouseenter(function(){
+    signBox.stop().slideDown(100);
+  })
+  signBox.mouseleave(function(){
+    signBox.stop().slideUp(200);
+  })
+});
+
+
+
 window.onload = function(){
   
 }
