3f8d42234142687820ad2fef710fc94b36b9af7f
seaho5599 <seaho5599@gmail.com>
2022-08-26 18:05:14 +0900

    header 작업 진행중

--- js/script.js
+++ js/script.js
@@ -1,21 +1,62 @@
 $(document).ready(function(){
+  let header = $('.header')
+  let depthLiA = $('.depth1 > li > a')
+  let dropLiA = $('.drop > li > a')
+  header.mouseenter(function(){
+    header.addClass('header-hv')
+    $('.logo').addClass('logo-hv')
+    $('.search').addClass('search-hv')
+    $('.cart').addClass('cart-hv')
+    $('.more-btn').addClass('more-btn-hv')
+    $('.num').addClass('num-hv')
+    depthLiA.css("color","#555")
+    dropLiA.css("color","#555")
+  })
+  header.mouseleave(function(){
+    header.removeClass('header-hv')
+    $('.logo').removeClass('logo-hv')
+    $('.search').removeClass('search-hv')
+    $('.cart').removeClass('cart-hv')
+    $('.more-btn').removeClass('more-btn-hv')
+    $('.num').removeClass('num-hv')
+    depthLiA.css("color","#fff")
+    dropLiA.css("color","#fff")
+  })
 
   let moreBtn = $('.more-btn');
   let moreBtnList = $('.more-btn-list');
   moreBtn.mouseenter(function(){
     moreBtnList.stop().slideDown(100);
   })
-  moreBtnList.mouseleave(function(){
+  moreBtn.mouseleave(function(){
     moreBtnList.stop().slideUp(200);
   })
-  let login = $('.login')
-  let signBox = $('.signbox')
+  let login = $('.login');
+  let signBox = $('.signbox');
+  let korea = $('.korea');
+  let langBox = $('.langbox');
   login.mouseenter(function(){
     signBox.stop().slideDown(100);
   })
-  signBox.mouseleave(function(){
+  login.mouseleave(function(){
     signBox.stop().slideUp(200);
   })
+  korea.mouseenter(function(){
+    langBox.stop().slideDown(100);
+  })
+  korea.mouseleave(function(){
+    langBox.stop().slideUp(200);
+  })
+  // let depthLiA = $('.depth1 > li >a');
+  // let depthLi = $('.depth1 > li')
+  // // let subMenu = $('.submenu');
+  // $.each(depthLi, function(index){
+  //     let depthLiA = $(this).find('>a');
+  //     let subMenu = $(this).find('.submenu');
+  //     $(this).mouseenter(function(){
+  //       subMenu.stop().slideToggle();
+  //     })
+  // })
 });
 
 
