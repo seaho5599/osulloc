39729dbf1f6721ea390e89207e57b3792dddd41f
seaho5599 <seaho5599@gmail.com>
2022-08-29 18:01:20 +0900

    visual swiper 작업중

--- js/script.js
+++ js/script.js
@@ -1,4 +1,20 @@
 $(document).ready(function(){
+
+  let swBanner = new Swiper('.sw-banner',{
+    direction: "vertical",
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    speed:800
+  })
+  let banner = $('.banner');
+  let bannerClose = $('.banner-close');
+  bannerClose.click(function(){
+    banner.fadeOut()
+  })
+
+
   let header = $('.header')
   let depthLiA = $('.depth1 > li > a')
   let dropLiA = $('.drop > li > a')
@@ -11,6 +27,7 @@ $(document).ready(function(){
     $('.num').addClass('num-hv')
     depthLiA.css("color","#555")
     dropLiA.css("color","#555")
+    $('.korea').addClass('korea-hv')
   })
   header.mouseleave(function(){
     header.removeClass('header-hv')
@@ -21,6 +38,7 @@ $(document).ready(function(){
     $('.num').removeClass('num-hv')
     depthLiA.css("color","#fff")
     dropLiA.css("color","#fff")
+    $('.korea').removeClass('korea-hv')
   })
 
   let moreBtn = $('.more-btn');
@@ -53,10 +71,27 @@ $(document).ready(function(){
   // $.each(depthLi, function(index){
   //     let depthLiA = $(this).find('>a');
   //     let subMenu = $(this).find('.submenu');
-  //     $(this).mouseenter(function(){
+  //     $(this).click(function(){
+  //       subMenu.hide()
   //       subMenu.stop().slideToggle();
   //     })
   // })
+
+  let swVisual = new Swiper('.sw-visual',{
+    
+    loop:true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+      
+    },
+    scrollbar: {
+      el: ".sw-visual-pg",
+      // hide: true,
+    },
+  })
+
+
 });
 
 
