43eb50749a7c5312a74ae75a384349cf5be616df
seaho5599 <seaho5599@gmail.com>
2022-09-02 17:57:26 +0900

    timer작업

--- js/script.js
+++ js/script.js
@@ -158,13 +158,13 @@ $(document).ready(function () {
               </a>
               </div>
         `;
-        
+
       });
       html += '';
       document.getElementById('item-list').innerHTML = html;
       let itemImg = $('.item-img')
-    
-      
+
+
       let swItem = new Swiper('.sw-item', {
         loop: true,
         slidesPerView: 5,
@@ -180,8 +180,34 @@ $(document).ready(function () {
       })
     })
     .catch(err => console.log(err))
+  // Set the date we're counting down to
+  let countDownDate = new Date("Jan 5, 2024 15:37:25").getTime();
 
-  
+  // Update the count down every 1 second
+  let x = setInterval(function () {
+
+    // Get today's date and time
+    let now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    let distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Output the result in an element with id="demo"
+    document.getElementById("news-time").innerHTML = +hours + " : " +
+      minutes + " : " + seconds;
+
+    // If the count down is over, write some text 
+    if (distance < 0) {
+      clearInterval(x);
+      document.getElementById("news-time").innerHTML = "EXPIRED";
+    }
+  }, 1000);
+
 });
 
 
